7037528a3d8e0759a56cc8ffc18975a310ec13b0
tiggywiggy <tiggywiggy@github.com>
2021-01-31 20:16:25 +0000

    First scraping using Reed

--- job_scraper.py
+++ job_scraper.py
@@ -1,21 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://uk.indeed.com/jobs?q=web+developer&l=devon'
+URL = 'https://www.reed.co.uk/jobs/web-developer-jobs-in-devon'
 
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-results = soup.find(id="resultsCol")
+results = soup.find(id='server-results')
 
-jobs = results.find_all('div', id = 'pj_ef4ab5c5b2eef673')
+jobs = results.find_all('article', class_='job-result')
+
+print('Found ', len(jobs), ' jobs! \n')
 
 for job in jobs:
-	title = job.find('h2', class_='title')
-    company= job.find('div', class_='company')
-    location = job.find('div', class_='location')
-	print(title)
-	print(company)
-	print(location)
-	print()
+    title = job.find('h3', class_='title')
+    company = job.find('a', class_='gtmJobListingPostedBy')
+    location = job.find(
+        'li', class_='location')
+    salary = job.find('li', class_='salary')
+    link = job.find('a')['href']
+    print('Title is:', title.text.strip())
+    print('Company is:', company.text.strip())
+    print('Location is:', location.text.strip())
+    print('Salary:', salary.text.strip())
+    #print('Apply here:', link)
+    print()
